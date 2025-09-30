36959457d21365f962e26fd2bc3f5edade88d83c
ShikharISM <shikharyadav885@gmail.com>
2025-10-01 02:01:40 +0530

    done

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,8 +8,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
-  const backendUrl = "http://localhost:4000";
-
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [search, setsearch] = useState("");
   const [searchvisible, setsearchvisible] = useState(false);
   const [cartitems, setcartitems] = useState({});
